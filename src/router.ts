38d0919fae0985dd82171f66d918a6a3d4b99a9b
Cody Biller <Sharpeleven91@gmail.com>
2023-03-12 01:26:16 -0500

    add router middleware validation

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { body } from 'express-validator';
+import { body, oneOf } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
@@ -12,21 +12,43 @@ router.put('/product/:id', body("name").isString(), handleInputErrors, (req, res
  
 });
 
-router.post('/product', body("name"), handleInputErrors, (req, res) => {});
+router.post('/product', body("name").isString(), handleInputErrors, (req, res) => {});
 router.delete('/product/:id', () => {});
 
 // update routes
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', body("name"), handleInputErrors, () => {});
-router.post('/update', body("name"), handleInputErrors, () => {});
+router.put('/update/:id', 
+    body("title").optional(), 
+    body("body").optional(),
+    body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]), 
+    body("version").optional(), 
+    handleInputErrors, 
+    () => {});
+
+router.post('/update',  
+    body("title").exists().isString(), 
+    body("body").exists().isString(),
+    handleInputErrors, 
+    () => {});
 router.delete('/update/:id', () => {});
 
 // update point routes
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', body("name"), handleInputErrors, () => {});
-router.post('/updatepoint', body("name"), handleInputErrors, () => {});
+router.put('/updatepoint/:id', 
+    body("name").optional().isString(),
+    body("description").optional().isString(), 
+    handleInputErrors, 
+    () => {});
+
+router.post('/updatepoint',     
+    body("name").optional().isString(),
+    body("description").optional().isString(),
+    body("updateId").exists().isString(),
+    handleInputErrors, 
+    () => {});
+
 router.delete('/updatepoint/:id', () => {});
 
 export default router;
