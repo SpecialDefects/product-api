1709adf2207ea3f3cf843c1dd599f042f8aaaae0
Cody Biller <Sharpeleven91@gmail.com>
2023-03-12 01:12:39 -0500

    add input error middleware

--- src/router.ts
+++ src/router.ts
@@ -1,49 +1,32 @@
 import { Router } from 'express'
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
 // product routes
-router.get('/product', body("name"), (req, res) => {
-    const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-     }
-});
+router.get('/product', body("name"), (req, res) => {});
 
 router.get('/product/:id', () => {});
-router.put('/product/:id', body("name").isString(), (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-    }
+router.put('/product/:id', body("name").isString(), handleInputErrors, (req, res) => {
+ 
 });
 
-router.post('/product', body("name"), (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-    }
-});
+router.post('/product', body("name"), handleInputErrors, (req, res) => {});
 router.delete('/product/:id', () => {});
 
 // update routes
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put('/update/:id', body("name"), handleInputErrors, () => {});
+router.post('/update', body("name"), handleInputErrors, () => {});
 router.delete('/update/:id', () => {});
 
 // update point routes
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
+router.put('/updatepoint/:id', body("name"), handleInputErrors, () => {});
+router.post('/updatepoint', body("name"), handleInputErrors, () => {});
 router.delete('/updatepoint/:id', () => {});
 
 export default router;
